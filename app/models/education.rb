564edda6c737bbf9a84f2b205c4cbba270e8af84
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-11-03 14:01:36 -0400

    Adds validations for all models

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,4 +1,7 @@
 class Education < ApplicationRecord
   belongs_to :student
 
+  validates :student_id, presence: true
+  validates :university_name, presence: true
+  validates :degree, presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,7 @@ class Student < ApplicationRecord
   has_many :capstones
   has_many :experiences
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
