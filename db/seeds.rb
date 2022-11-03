f3e9fc5401549c3b4701faea6f2d52cba0f32c40
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-11-03 14:48:31 -0400

    Adds seeds for 2 students, adds index action and route for students

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+student = Student.new(
+  first_name: "Bob",
+  last_name: "Burgers",
+  email: "bob@test.com",
+  phone_number: 1234567890,
+  short_bio: "This is a great bio so please hire me :).",
+  linkedin_url: "htp://linkedin.test/bestuser",
+  twitter_handle: "bobsome",
+  website_blog_url: "bobblog.test",
+  resume_url: "bobresume.test",
+  github_url: "bobgithub.test",
+  photo: "bob.jpeg",
+)
+student.save
+
+student = Student.new(
+  first_name: "Lucy",
+  last_name: "O'ball",
+  email: "lucy@test.com",
+  phone_number: 1235467890,
+  short_bio: "This is an awesome bio, that is why I am a great candidate for any job.",
+  linkedin_url: "htp://linkedin.test/lucyoball",
+  twitter_handle: "lucydious",
+  website_blog_url: "lucyblog.test",
+  resume_url: "lucyresume.test",
+  github_url: "lucygithub.test",
+  photo: "lucy.jpeg",
+)
+student.save
